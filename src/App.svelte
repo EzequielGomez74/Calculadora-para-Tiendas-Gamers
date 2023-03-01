7080d593e545dd160f510455226c38be42b8d628
EzequielGomez74 <ezequiel.canob@gmail.com>
2023-03-01 14:57:12 -0300

    primer commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+
+import Calculadora from "./calculadora.svelte";
+import Navbar from "./navbar.svelte";
+import Bienvenida from "./bienvenida.svelte";
+import Preguntas from "./preguntas.svelte";
+import Footer from "./footer.svelte";
+import Comentarios from "./comentarios.svelte";
+
+</script>
+
+<main>       
+        <Navbar />	
+        <Bienvenida />
+        <Calculadora />
+        <Comentarios />
+        <Preguntas />
+        <Footer />
+</main>
+
+<style>
+
+
+
+
+</style>
---
+++ src/footer.svelte
@@ -0,0 +1,34 @@
+<script></script>
+
+
+
+<footer id="contacto" class="p-5 text-center text-light" style="margin-top: 15%;">
+
+<h3 style="color:salmon;">Calculadora para Tiendas Gamers</h3>
+<h4>Ezequiel Gomez</h4>
+<br>
+<h3 style="color:salmon;">Contacto</h3>
+<h4>ezequiel.canob@gmail.com</h4>
+
+<div class="mt-4" style="font-size:28px">
+
+    <a href="https://www.linkedin.com/in/eze-gomez/" target="_blank" class="text-light"><i class="bi bi-linkedin mx-3"></i></a>
+    <a href="https://github.com/EzequielGomez74"  target="_blank" class="text-light"><i class="bi bi-github mx-3"></i></a>
+
+</div>
+
+</footer>
+
+
+<style>
+
+footer
+
+{
+
+    background-color: rgb(10, 10, 20, 0.5);
+    border-top: 3px salmon solid;
+
+}
+
+</style>
